1f231b6941746efad066a28473e6c6cb5bf52407
Yousuf <channel.redfox@gmail.com>
2020-04-19 00:15:38 -0400

    test for count online

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ if(!(config.get('jwtPrivateKey'))){
     process.exit(1);
 }
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+});
+
 app.set('view engine', 'pug');
 app.use(express.static('views'));
 app.use(express.json());
