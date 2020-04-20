8857721adf9ca65f3179eddb73e44745053ce7aa
Yousuf <channel.redfox@gmail.com>
2020-04-20 01:12:10 -0400

    added more styling + added username

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+    const token = jwt.sign({ username: this.username }, config.get('jwtPrivateKey'));
     return token;
 }
 
--- routes/gameServer.js
+++ routes/gameServer.js
@@ -4,9 +4,17 @@ const cookieParser = require('cookie-parser');
 const auth = require('../middleware/auth');
 
 router.use(cookieParser());
+let username;
+
+router.post('/', auth, (req, res) =>{
+    username = req.body.info.username;
+    res.end();
+});
 
 router.get('/', auth, (req, res) =>{
-    res.render('game', {});
+    let ca = req.cookies.access_token;
+    let base64Url = ca.split('.')[1];
+    res.render('game', {username : username});
 });
 
 
