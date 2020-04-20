d24759617838a6df094149861a603a8d22008ec7
Yousuf <channel.redfox@gmail.com>
2020-04-20 16:30:11 -0400

    se

--- routes/clickPotato.js
+++ routes/clickPotato.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) =>{
     user.Score = user.Score + Number(req.body.defaultValue);
     console.log(user.Score);
     await user.save();
-    res.end(user.Score);
+    res.send(user.Score).end();
 });
 
 module.exports = router;
