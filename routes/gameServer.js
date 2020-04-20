85a5bc2333bc663522b615ddb53d00960a73d1bd
Yousuf <channel.redfox@gmail.com>
2020-04-20 01:19:25 -0400

    testing decoding

--- routes/gameServer.js
+++ routes/gameServer.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const jwtDecode = require('jwt-decode');
 const cookieParser = require('cookie-parser');
 const auth = require('../middleware/auth');
 
 router.use(cookieParser());
 
 router.get('/', auth, (req, res) =>{
-    let ca = req.cookies.access_token;
-    let base64Url = ca.split('.')[1];
-    let decodedValue = JSON.parse(atob(base64Url));
-    console.log(decodedValue);
+    const token = req.cookies.access_token;
+    let decoded = jwt_decode(token);
+    console.log(decoded);
     res.render('game', {username : username});
 });
 
