78b0c38d6de33a46954299403b2321e4b9f7e392
Yousuf <channel.redfox@gmail.com>
2020-04-20 01:16:35 -0400

    d

--- routes/gameServer.js
+++ routes/gameServer.js
@@ -8,7 +8,7 @@ router.use(cookieParser());
 router.get('/', auth, (req, res) =>{
     let ca = req.cookies.access_token;
     let base64Url = ca.split('.')[1];
-    let decodedValue = JSON.parse(window.atob(base64Url));
+    let decodedValue = JSON.parse(atob(base64Url));
     console.log(decodedValue);
     res.render('game', {username : username});
 });
