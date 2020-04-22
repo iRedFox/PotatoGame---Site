0cf27cbe6160017c770c879a0937b40e1f0c3bc8
Yousuf <channel.redfox@gmail.com>
2020-04-21 22:20:51 -0400

    preventing Autoclicker

--- views/game.js
+++ views/game.js
@@ -3,6 +3,7 @@ console.log('The client javascript code is running..');
 let tutorial = document.getElementById('tutorial');
 let c2d = document.getElementById('potatoHitBox');
 let logoutButton = document.getElementById('logout-button');
+let canClick = true;
 
 c2d.width = 105;
 c2d.height = 125;
@@ -63,31 +64,46 @@ c2d.addEventListener("mouseout", mouseMoveOut, false);
 let button_click = document.getElementById('potatoHitBox');
 button_click.onclick = countClicks;
 scoreInfo = { defaultValue: 1 };
-async function countClicks(){
-	// Move the image randomly.
-	let x = Math.floor(Math.random()*600);
-	let y = Math.floor(Math.random()*400);
-	button_click.style.top = x + 'px';
-    button_click.style.left = y + 'px';
-    const url = 'http://134.122.81.113:80/clickRegistry';
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(scoreInfo),
-        headers:{
-            'Content-Type': 'application/json'
-        },
-    }
-    fetch(url, options)
-    .then((response) => {
-        return response.text();
-    })
-    .then((data) => {
-        let currentScore = 0;
-        currentScore = data;
-        console.log('currentScore: ' + currentScore);
-        const points = document.getElementById('coin-count').innerHTML = currentScore;
-    });
 
+setInterval(changeCanClick, 2000);
+
+if(canClick){
+    
+    async function countClicks(){
+        // Move the image randomly.
+        let x = Math.floor(Math.random()*600);
+        let y = Math.floor(Math.random()*400);
+        button_click.style.top = x + 'px';
+        button_click.style.left = y + 'px';
+        const url = 'http://134.122.81.113:80/clickRegistry';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(scoreInfo),
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        }
+        fetch(url, options)
+        .then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            let currentScore = 0;
+            currentScore = data;
+            console.log('currentScore: ' + currentScore);
+            const points = document.getElementById('coin-count').innerHTML = currentScore;
+        });
+        console.log(canClick);
+        canClick = false;
+        console.log(canClick);
+    
+    }
+}else{
+    return;
+}
+
+function changeCanClick(){
+    let canClick = true;
 }
 
 
