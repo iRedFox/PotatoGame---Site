b9d23bd7a2311a5a471b4f8399a647f5725a1ee6
Yousuf <channel.redfox@gmail.com>
2020-06-19 04:53:25 -0400

    fixed issue

--- views/store.js
+++ views/store.js
@@ -1,39 +1,35 @@
-
-function checkCreation(element){
-    if(!element) return;
-}
-
 const ghostButton = document.getElementById('ghost-buy');
-checkCreation(ghostButton);
 const ghostChoice = document.getElementById('ghost-choice');
-checkCreation(ghostChoice);
 const defaultChoice = document.getElementById('default-choice');
 
-
-ghostButton.onclick = async () => {
-    newSkin = { skin: '/potatoClickTeto.gif' };
-    const url = 'http://b6a6s.io/store';
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(newSkin),
-        headers:{
-            'Content-Type': 'application/json'
-        },
+if(ghostButton){
+    ghostButton.onclick = async () => {
+        newSkin = { skin: '/potatoClickTeto.gif' };
+        const url = 'http://b6a6s.io/store';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(newSkin),
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        }
+        fetch(url, options)
     }
-    fetch(url, options)
 }
 
-ghostChoice.onclick = async () => {
-    newSkin = { skin: '/potatoClickTeto.gif' };
-    const url = 'http://b6a6s.io/store';
-    const options = {
-        method: 'PUT',
-        body: JSON.stringify(newSkin),
-        headers:{
-            'Content-Type': 'application/json'
-        },
+if(ghostChoice){
+    ghostChoice.onclick = async () => {
+        newSkin = { skin: '/potatoClickTeto.gif' };
+        const url = 'http://b6a6s.io/store';
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(newSkin),
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        }
+        fetch(url, options)
     }
-    fetch(url, options)
 }
 
 defaultChoice.onclick = async () => {
